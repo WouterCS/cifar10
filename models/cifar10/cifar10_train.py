bc6c05529478fbdc23a8029df7bf728765e6dc8b
Wouter Uijens <wouter.cs@gmail.com>
2017-07-11 19:28:14 +0200

    Added accuracy testing (attempt 28)

--- models/cifar10/cifar10_train.py
+++ models/cifar10/cifar10_train.py
@@ -109,8 +109,8 @@ def train():
     with tf.train.MonitoredTrainingSession(
         checkpoint_dir=FLAGS.train_dir,
         hooks=[tf.train.CheckpointSaverHook('/results/savedWeights/', save_steps = 1000), 
-               tf.train.StopAtStepHook(last_step=FLAGS.max_steps),
-               tf.train.NanTensorHook(loss)],#,_LoggerHook()],
+               tf.train.StopAtStepHook(last_step=FLAGS.max_steps)],#,tf.train.NanTensorHook(loss),_LoggerHook()],
+               
                
         config=tf.ConfigProto(
             log_device_placement=FLAGS.log_device_placement)) as mon_sess:
