91fc6551994d5efcf5ae8986fc2a3ef07d592d49
Wouter Uijens <wouter.cs@gmail.com>
2017-07-11 15:26:06 +0200

    print file path looking for (attempt)

--- DockerScripts/DockerCreation/ondockerSetUp-stage2.bash
+++ DockerScripts/DockerCreation/ondockerSetUp-stage2.bash
@@ -2,5 +2,5 @@ mkdir -p /results
 
 cd /models/cifar10
 
-python -c "import  cifar10_train as model; model.train()" 2>&1  | tee /results/log.txt
+python -c "import  cifar10_train as model; model.train()" #2>&1  | tee /results/log.txt
 #python -c "import RFNN.trainnonlin.mod_training as train; train.run()" 2>&1  | tee /results/log.txt
