ad8fded4755896b68c10905be9d9d73269e3bdef
Wouter Uijens <wouter.cs@gmail.com>
2017-07-11 14:02:03 +0200

    cd instead of long import

--- DockerScripts/DockerCreation/ondockerSetUp-stage1.bash
+++ DockerScripts/DockerCreation/ondockerSetUp-stage1.bash
@@ -1,3 +1,2 @@
 #apt-get update
 #apt-get -y install python-tk
-git clone https://github.com/tensorflow/models.git
--- DockerScripts/DockerCreation/ondockerSetUp-stage2.bash
+++ DockerScripts/DockerCreation/ondockerSetUp-stage2.bash
@@ -1,4 +1,6 @@
 mkdir -p /results
 
-python -c "import  models.tutorials.image.cifar10.cifar10_train as model; model.train()" 2>&1  | tee /results/log.txt
+cd /models/tutorials/image/cifar10
+
+python -c "import  cifar10_train as model; model.train()" 2>&1  | tee /results/log.txt
 #python -c "import RFNN.trainnonlin.mod_training as train; train.run()" 2>&1  | tee /results/log.txt
