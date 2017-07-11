72f49d980009edef0209d0d5233bd7a3ac37d25c
Wouter Uijens <wouter.cs@gmail.com>
2017-07-11 15:24:31 +0200

    print file path looking for (attempt4)

--- models/cifar10/cifar10.py
+++ models/cifar10/cifar10.py
@@ -379,6 +379,7 @@ def train(total_loss, global_step):
 
 
 def maybe_download_and_extract():
+  print('start maybe download')
   """Download and extract the tarball from Alex's website."""
   dest_directory = FLAGS.data_dir
   if not os.path.exists(dest_directory):
