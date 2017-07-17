7c5d5faaa3e1db790a38d71555c7f14308e5bc9d
Wouter Uijens <wouter.cs@gmail.com>
2017-07-17 19:02:24 +0200

    Try to store and eload weights (atempt 6)

--- models/cifar10/cifar10_eval.py
+++ models/cifar10/cifar10_eval.py
@@ -147,9 +147,9 @@ def evaluate():
 
 def main(argv=None):  # pylint: disable=unused-argument
   cifar10.maybe_download_and_extract()
-  if tf.gfile.Exists(FLAGS.eval_dir):
-    tf.gfile.DeleteRecursively(FLAGS.eval_dir)
-  tf.gfile.MakeDirs(FLAGS.eval_dir)
+  if tf.gfile.Exists('/tmp/cifar10_eval'):
+    tf.gfile.DeleteRecursively('/tmp/cifar10_eval')
+  tf.gfile.MakeDirs('/tmp/cifar10_eval')
   evaluate()
 
 
