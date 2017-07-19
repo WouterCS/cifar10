27285c2bffa6f813a4f10cb1e34d2ac3d1617952
Wouter Uijens <wouter.cs@gmail.com>
2017-07-19 19:01:20 +0200

    Nightly run, 4 options of having relu and identity in conv and FC with 1,000,000 batches

--- DockerScripts/DockerCreation/ondockerSetUp-stage2.bash
+++ DockerScripts/DockerCreation/ondockerSetUp-stage2.bash
@@ -3,7 +3,9 @@ mkdir -p /results
 cd /models/cifar10
 
 python -c "convNonLin = 'identity'; FCnonLin = 'identity'; print('convNonLin: %s, FCnonLin: %s' % (convNonLin, FCnonLin)); import  cifar10_train as model; model.main(convNonLin, FCnonLin);import cifar10_eval as eval; eval.main(convNonLin, FCnonLin)" 2>&1  | tee /results/log.txt
-python -c "convNonLin = 'relu'; FCnonLin = 'identity'; print('convNonLin: %s, FCnonLin: %s' % (convNonLin, FCnonLin)); import  cifar10_train as model; model.main(convNonLin, FCnonLin);import cifar10_eval as eval; eval.main(convNonLin, FCnonLin)" 2>&1  | tee /results/log.txt
+python -c "convNonLin = 'relu'; FCnonLin = 'identity'; print('convNonLin: %s, FCnonLin: %s' % (convNonLin, FCnonLin)); import  cifar10_train as model; model.main(convNonLin, FCnonLin);import cifar10_eval as eval; eval.main(convNonLin, FCnonLin)" 2>&1  | tee /results/log2.txt
+python -c "convNonLin = 'identity'; FCnonLin = 'relu'; print('convNonLin: %s, FCnonLin: %s' % (convNonLin, FCnonLin)); import  cifar10_train as model; model.main(convNonLin, FCnonLin);import cifar10_eval as eval; eval.main(convNonLin, FCnonLin)" 2>&1  | tee /results/log.txt
+python -c "convNonLin = 'relu'; FCnonLin = 'relu'; print('convNonLin: %s, FCnonLin: %s' % (convNonLin, FCnonLin)); import  cifar10_train as model; model.main(convNonLin, FCnonLin);import cifar10_eval as eval; eval.main(convNonLin, FCnonLin)" 2>&1  | tee /results/log2.txt
 #cat /models/cifar10/cifar10_train.py 2>&1  | tee /results/log.txt
 
 #python -c "import RFNN.trainnonlin.mod_training as train; train.run()" 2>&1  | tee /results/log.txt
