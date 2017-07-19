5f10a795ab69ee9b15a6e0d8811d6af879226f18
Wouter Uijens <wouter.cs@gmail.com>
2017-07-19 16:07:58 +0200

    Try to store and reload weights (atempt 29)

--- DockerScripts/DockerCreation/ondockerSetUp-stage2.bash
+++ DockerScripts/DockerCreation/ondockerSetUp-stage2.bash
@@ -3,7 +3,6 @@ mkdir -p /results
 cd /models/cifar10
 
 python -c "import  cifar10_train as model; model.main();import cifar10_eval as eval; eval.main()" 2>&1  | tee /results/log.txt
-python -c "import cifar10_eval as eval; eval.main()" 2>&1  | tee /results/log.txt
 #cat /models/cifar10/cifar10_train.py 2>&1  | tee /results/log.txt
 
 #python -c "import RFNN.trainnonlin.mod_training as train; train.run()" 2>&1  | tee /results/log.txt
