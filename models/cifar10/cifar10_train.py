efe826b1743fe2d2bd8fc4b8c59b7cba4678e43c
Wouter Uijens <wouter.cs@gmail.com>
2017-07-19 12:51:32 +0200

    Try to store and reload weights (atempt 7)

--- models/cifar10/cifar10_train.py
+++ models/cifar10/cifar10_train.py
@@ -49,6 +49,8 @@ FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string('train_dir', '/tmp/cifar10_train',
                            """Directory where to write event logs """
                            """and checkpoint.""")
+tf.app.flags.DEFINE_string('eval_dir', '/tmp/cifar10_eval',
+                           """Directory where to write event logs.""")
 tf.app.flags.DEFINE_integer('max_steps', 1000, #1000000
                             """Number of batches to run.""")
 tf.app.flags.DEFINE_boolean('log_device_placement', False,
