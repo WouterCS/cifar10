77a2aca9dd9765df97371162c87f8e05fb950817
Wouter Uijens <wouter.cs@gmail.com>
2017-07-19 15:54:25 +0200

    Try to store and reload weights (atempt 25)

--- models/cifar10/cifar10_eval.py
+++ models/cifar10/cifar10_eval.py
@@ -58,7 +58,6 @@ def createFlags():
     createOrUpdateFlag(tf.app.flags.DEFINE_integer, 'eval_interval_secs', 60 * 5, """How often to run the eval.""")
     createOrUpdateFlag(tf.app.flags.DEFINE_integer, 'num_examples', 10000, """Number of examples to run.""")
     createOrUpdateFlag(tf.app.flags.DEFINE_boolean, 'run_once', True, """Whether to run eval only once.""")
-    FLAGS = tf.app.flags.FLAGS
 
 
 def eval_once(saver, summary_writer, top_k_op, summary_op):
@@ -71,7 +70,7 @@ def eval_once(saver, summary_writer, top_k_op, summary_op):
     summary_op: Summary op.
   """
   with tf.Session() as sess:
-    ckpt = tf.train.get_checkpoint_state(FLAGS.checkpoint_dir)
+    ckpt = tf.train.get_checkpoint_state(tf.app.flags.FLAGS.checkpoint_dir)
     if ckpt and ckpt.model_checkpoint_path:
       # Restores from checkpoint
       saver.restore(sess, ckpt.model_checkpoint_path)
@@ -91,9 +90,9 @@ def eval_once(saver, summary_writer, top_k_op, summary_op):
         threads.extend(qr.create_threads(sess, coord=coord, daemon=True,
                                          start=True))
 
-      num_iter = int(math.ceil(FLAGS.num_examples / FLAGS.batch_size))
+      num_iter = int(math.ceil(tf.app.flags.FLAGS.num_examples / tf.app.flags.FLAGS.batch_size))
       true_count = 0  # Counts the number of correct predictions.
-      total_sample_count = num_iter * FLAGS.batch_size
+      total_sample_count = num_iter * tf.app.flags.FLAGS.batch_size
       step = 0
       while step < num_iter and not coord.should_stop():
         predictions = sess.run([top_k_op])
@@ -116,11 +115,10 @@ def eval_once(saver, summary_writer, top_k_op, summary_op):
 
 
 def evaluate():
-  print(FLAGS.eval_data)
   """Eval CIFAR-10 for a number of steps."""
   with tf.Graph().as_default() as g:
     # Get images and labels for CIFAR-10.
-    eval_data = FLAGS.eval_data == 'test'
+    eval_data = tf.app.flags.FLAGS.eval_data == 'test'
     images, labels = cifar10.inputs(eval_data=eval_data)
 
     # Build a Graph that computes the logits predictions from the
@@ -139,22 +137,21 @@ def evaluate():
     # Build the summary operation based on the TF collection of Summaries.
     summary_op = tf.summary.merge_all()
 
-    summary_writer = tf.summary.FileWriter(FLAGS.eval_dir, g)
+    summary_writer = tf.summary.FileWriter(tf.app.flags.FLAGS.eval_dir, g)
 
     while True:
       eval_once(saver, summary_writer, top_k_op, summary_op)
-      if FLAGS.run_once:
+      if tf.app.flags.FLAGS.run_once:
         break
-      time.sleep(FLAGS.eval_interval_secs)
+      time.sleep(tf.app.flags.FLAGS.eval_interval_secs)
 
 
 def main(argv=None):  # pylint: disable=unused-argument
   createFlags()
-  print(FLAGS.eval_data)
   cifar10.maybe_download_and_extract()
-  if tf.gfile.Exists(FLAGS.eval_dir):
-    tf.gfile.DeleteRecursively(FLAGS.eval_dir)
-  tf.gfile.MakeDirs(FLAGS.eval_dir)
+  if tf.gfile.Exists(tf.app.flags.FLAGS.eval_dir):
+    tf.gfile.DeleteRecursively(tf.app.flags.FLAGS.eval_dir)
+  tf.gfile.MakeDirs(tf.app.flags.FLAGS.eval_dir)
   evaluate()
 
 
