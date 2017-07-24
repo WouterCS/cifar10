d5332c4b1a2661c94a2c9f969d482f9beafef128
Wouter Uijens <wouter.cs@gmail.com>
2017-07-24 20:08:13 +0200

    Encapsulated hyperparameters in python function, post results from separate runs in separate folders and generate conversion plots (attempt 9)

---
+++ models/cifar10/cifar10_params.py
@@ -0,0 +1,50 @@
+import os.path
+
+def main(runNum):
+    
+    directory = '/results/run-%d' % runNum
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    
+    class hyperParameters:
+        def __init__(self):
+            self.datasetname = 'CIFAR-10'
+            self.FCnonLin = 'identity' # 'relu'   'powMagnitude'
+            self.FCnonLinMag = 0.9
+            self.convNonLin = 'identity' # 'relu'   'powMagnitude'
+            self.convNonLinMag = 0.9
+            self.poolingFun = 'max-pool' # 'average-pool'
+            self.max_steps = 1000000
+            self.eval_frequency = 1000
+    
+    hyperParam = hyperParameters()
+    hyperParam.max_steps = 300
+    hyperParam.eval_frequency = 100
+
+    if runNum == 1:
+        hyperParam.convNonLin = 'powMagnitude'
+        hyperParam.convNonLinMag = 0.9
+    if runNum == 2:
+        hyperParam.convNonLin = 'relu'
+    if runNum == 3:
+        hyperParam.convNonLin = 'identity'
+    if runNum == 3:
+        hyperParam.convNonLin = 'powMagnitude'
+        hyperParam.convNonLinMag = 0.8
+        
+    return hyperParam
+    
+def createReadMe(hyperParam, directory):
+    with open(directory + '/README.txt', 'wb') as f:
+        print('Dataset: %s' % hyperParam.datasetname, file = f)
+        if hyperParam.FCnonLin == 'powMagnitude':
+            print('FC non-linearity: %s, with power: %f' % (hyperParam.FCnonLin, hyperParam.FCnonLinMag), file = f)
+        else:
+            print('FC non-linearity: %s' % hyperParam.FCnonLin, file = f)
+        if hyperParam.convNonLin == 'powMagnitude':
+            print('Conv non-linearity: %s, with power: %f' % (hyperParam.convNonLin, hyperParam.convNonLinMag), file = f)
+        else:
+            print('Conv non-linearity: %s' % hyperParam.convNonLin, file = f)
+        print('Pooling function is: %s' % hyperParam.poolingFun, file = f)
+        print('Maximum number of steps is: %d' % hyperParam.max_steps, file = f)
+        print('Evaluation every %d steps.' % hyperParam.eval_frequency, file = f)
