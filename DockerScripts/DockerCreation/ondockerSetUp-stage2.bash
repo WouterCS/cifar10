717356438ab31c08bac94d740c9a4bd001119ad4
Wouter Uijens <wouter.cs@gmail.com>
2017-07-24 20:37:54 +0200

    Encapsulated hyperparameters in python function, post results from separate runs in separate folders and generate conversion plots (attempt 23)

--- DockerScripts/DockerCreation/ondockerSetUp-stage2.bash
+++ DockerScripts/DockerCreation/ondockerSetUp-stage2.bash
@@ -5,6 +5,11 @@ cd /models/cifar10
 DIRECTORY='/results/run-1'
 mkdir -p $DIRECTORY
 python -c "import cifar10_params; hyperParam = cifar10_params.main(1); import cifar10_train; cifar10_train.main(hyperParam, '${DIRECTORY}')" 2>&1  | tee "${DIRECTORY}/log.txt"
-#cat /models/cifar10/cifar10_train.py 2>&1  | tee /results/log.txt
 
-#python -c "import RFNN.trainnonlin.mod_training as train; train.run()" 2>&1  | tee /results/log.txt
+DIRECTORY='/results/run-2'
+mkdir -p $DIRECTORY
+python -c "import cifar10_params; hyperParam = cifar10_params.main(2); import cifar10_train; cifar10_train.main(hyperParam, '${DIRECTORY}')" 2>&1  | tee "${DIRECTORY}/log.txt"
+
+DIRECTORY='/results/run-3'
+mkdir -p $DIRECTORY
+python -c "import cifar10_params; hyperParam = cifar10_params.main(3); import cifar10_train; cifar10_train.main(hyperParam, '${DIRECTORY}')" 2>&1  | tee "${DIRECTORY}/log.txt"
