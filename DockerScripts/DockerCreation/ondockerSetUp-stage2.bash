7611c13f9ed852a8e6a592a007cb65ca310618b3
Wouter Uijens <wouter.cs@gmail.com>
2017-07-24 19:48:47 +0200

    Encapsulated hyperparameters in python function, post results from separate runs in separate folders and generate conversion plots (attempt 3)

--- DockerScripts/DockerCreation/ondockerSetUp-stage2.bash
+++ DockerScripts/DockerCreation/ondockerSetUp-stage2.bash
@@ -4,7 +4,7 @@ cd /models/cifar10
 
 DIRECTORY='/results/run-1'
 mkdir -p $DIRECTORY
-python -c "hyperParam = params.main(1); cifar10_train.main(hyperParam, ${DIRECTORY})" 2>&1  | tee "${DIRECTORY}/log.txt"
+python -c "hyperParam = params.main(1); cifar10_train.main(hyperParam, \'${DIRECTORY}\')" 2>&1  | tee "${DIRECTORY}/log.txt"
 #cat /models/cifar10/cifar10_train.py 2>&1  | tee /results/log.txt
 
 #python -c "import RFNN.trainnonlin.mod_training as train; train.run()" 2>&1  | tee /results/log.txt
