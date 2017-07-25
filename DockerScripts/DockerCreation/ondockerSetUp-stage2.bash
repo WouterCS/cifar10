cd6e8b1d7f2275adae4f63a14cabd5e5b6b363b2
Wouter Uijens <wouter.cs@gmail.com>
2017-07-25 13:53:13 +0200

    Run double identity, relu identity and 0.8 powmagnitude for 1,000,000 steps

--- DockerScripts/DockerCreation/ondockerSetUp-stage2.bash
+++ DockerScripts/DockerCreation/ondockerSetUp-stage2.bash
@@ -2,10 +2,6 @@ mkdir -p /results
 
 cd /models/cifar10
 
-DIRECTORY='/results/run-1'
-mkdir -p $DIRECTORY
-python -c "import cifar10_params; hyperParam = cifar10_params.main(1); import cifar10_train; cifar10_train.main(hyperParam, '${DIRECTORY}')" 2>&1  | tee "${DIRECTORY}/log.txt"
-
 DIRECTORY='/results/run-2'
 mkdir -p $DIRECTORY
 python -c "import cifar10_params; hyperParam = cifar10_params.main(2); import cifar10_train; cifar10_train.main(hyperParam, '${DIRECTORY}')" 2>&1  | tee "${DIRECTORY}/log.txt"
