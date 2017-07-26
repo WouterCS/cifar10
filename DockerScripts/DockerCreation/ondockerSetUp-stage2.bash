ad960cead7499513738170721cc138cd9ffed3f6
Wouter Uijens <wouter.cs@gmail.com>
2017-07-26 11:06:57 +0200

    Run 0.8 powmagnitude for 1,000,000 steps (attempt 4)

--- DockerScripts/DockerCreation/ondockerSetUp-stage2.bash
+++ DockerScripts/DockerCreation/ondockerSetUp-stage2.bash
@@ -2,6 +2,8 @@ mkdir -p /results
 
 cd /models/cifar10
 
+python -c "from tensorflow.contrib.layers.python.layers.embedding_ops import *"
+
 DIRECTORY='/results/run-4'
 mkdir -p $DIRECTORY
 python -c "import cifar10_params; hyperParam = cifar10_params.main(1,'${DIRECTORY}'); import cifar10_train; cifar10_train.main(hyperParam, '${DIRECTORY}')" 2>&1  | tee "${DIRECTORY}/log.txt"
