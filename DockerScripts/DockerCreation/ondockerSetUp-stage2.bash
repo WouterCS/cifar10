217bbc766e5a494f43f38e0df0588a6dae4a352f
Wouter Uijens <wouter.cs@gmail.com>
2017-07-26 10:33:40 +0200

    Run 0.8 powmagnitude for 1,000,000 steps

--- DockerScripts/DockerCreation/ondockerSetUp-stage2.bash
+++ DockerScripts/DockerCreation/ondockerSetUp-stage2.bash
@@ -2,14 +2,6 @@ mkdir -p /results
 
 cd /models/cifar10
 
-DIRECTORY='/results/run-2'
-mkdir -p $DIRECTORY
-python -c "import cifar10_params; hyperParam = cifar10_params.main(2); import cifar10_train; cifar10_train.main(hyperParam, '${DIRECTORY}')" 2>&1  | tee "${DIRECTORY}/log.txt"
-
-DIRECTORY='/results/run-3'
-mkdir -p $DIRECTORY
-python -c "import cifar10_params; hyperParam = cifar10_params.main(3); import cifar10_train; cifar10_train.main(hyperParam, '${DIRECTORY}')" 2>&1  | tee "${DIRECTORY}/log.txt"
-
 DIRECTORY='/results/run-4'
 mkdir -p $DIRECTORY
 python -c "import cifar10_params; hyperParam = cifar10_params.main(4); import cifar10_train; cifar10_train.main(hyperParam, '${DIRECTORY}')" 2>&1  | tee "${DIRECTORY}/log.txt"
