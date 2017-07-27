de9bd93eeed5427d0eb865257ac034c66561e5e1
Wouter Uijens <wouter.cs@gmail.com>
2017-07-27 14:18:07 +0200

    Big run with average pooling (identity, relu, pow-mag-0.9, pow-mag-0.8), add constant updated graph

--- DockerScripts/DockerCreation/ondockerSetUp-stage2.bash
+++ DockerScripts/DockerCreation/ondockerSetUp-stage2.bash
@@ -5,11 +5,6 @@ cd /models/cifar10
 #python -c "from tensorflow.contrib.layers.python.layers.embedding_ops import *"
 #cat /usr/local/lib/python2.7/dist-packages/tensorflow/contrib/layers/python/layers/embedding_ops.py
 
-numRun='5'
-DIRECTORY='/results/run-'$numRun
-mkdir -p $DIRECTORY
-echo $"${DIRECTORY}/log.txt"
-python -c "import cifar10_params; hyperParam = cifar10_params.main(${numRun},'${DIRECTORY}'); import cifar10_train; cifar10_train.main(hyperParam, '${DIRECTORY}')" 2>&1  | tee "${DIRECTORY}/log.txt"
 
 numRun='6'
 DIRECTORY='/results/run-'$numRun
