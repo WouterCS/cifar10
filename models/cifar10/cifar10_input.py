5884fd6e01d356c6f293d9fb76f2395a265a0991
Wouter Uijens <wouter.cs@gmail.com>
2017-07-27 14:20:26 +0200

    Big run with average pooling (identity, relu, pow-mag-0.9, pow-mag-0.8), add constant updated graph (attempt 5)

--- models/cifar10/cifar10_input.py
+++ models/cifar10/cifar10_input.py
@@ -123,14 +123,14 @@ def _generate_image_and_label_batch(image, label, min_queue_examples,
         batch_size=batch_size,
         num_threads=num_preprocess_threads,
         capacity=min_queue_examples + 3 * batch_size,
-        min_after_dequeue=min_queue_examples)
+        min_after_dequeue=min_queue_examples,
+        seed=seed)
   else:
     images, label_batch = tf.train.batch(
         [image, label],
         batch_size=batch_size,
         num_threads=num_preprocess_threads,
-        capacity=min_queue_examples + 3 * batch_size,
-        seed=seed)
+        capacity=min_queue_examples + 3 * batch_size)
 
   # Display the training images in the visualizer.
   tf.summary.image('images', images)
