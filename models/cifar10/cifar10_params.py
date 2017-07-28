386a2e0e38a23e6860f8c2ed1b18f04e00fec8c1
Wouter Uijens <wouter.cs@gmail.com>
2017-07-28 18:12:14 +0200

    Weekend run: one big run at 0.7, rest random pows (attempt 7)

--- models/cifar10/cifar10_params.py
+++ models/cifar10/cifar10_params.py
@@ -30,7 +30,7 @@ def main(runNum, directory):
     
     hyperParam = hyperParameters()
     hyperParam.poolingFun = 'average-pool'
-    hyperParam.INITIAL_LEARNING_RATE = 0.2
+    hyperParam.INITIAL_LEARNING_RATE = 0.1
     
     if runNum == 0:
         print('run zero')
