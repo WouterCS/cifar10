eb2906b217d5787be5d34c78f2b719266d52ad60
Wouter Uijens <wouter.cs@gmail.com>
2017-07-31 15:31:21 +0200

    Try magnitudes 3 with progressively lower learning rate

--- models/cifar10/cifar10_params.py
+++ models/cifar10/cifar10_params.py
@@ -30,7 +30,7 @@ def main(runNum, directory):
     
     hyperParam = hyperParameters()
     hyperParam.poolingFun = 'average-pool'
-    hyperParam.INITIAL_LEARNING_RATE = 0.1
+    hyperParam.INITIAL_LEARNING_RATE = 0.1 / (2 ** runNum)
         
     hyperParam.max_steps = 30000
     hyperParam.convNonLin = 'powMagnitude'
