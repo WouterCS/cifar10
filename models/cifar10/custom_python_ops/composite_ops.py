7f91fe11f7bcf2d4cf8f6fb6981d0c1ccd1e4254
Wouter Uijens <wouter.cs@gmail.com>
2017-08-01 16:58:57 +0200

    Try absolute value in the new setting, continue running with lower lr (attempt 3)

--- models/cifar10/custom_python_ops/composite_ops.py
+++ models/cifar10/custom_python_ops/composite_ops.py
@@ -4,29 +4,12 @@ from custom_ops import tf_arctan2
 
 
 def tf_angle(c):
-    return tf_arctan2(tf.imag(c), tf.real(c))
+    return tf.atan2(tf.imag(c), tf.real(c))
 
 def sqrtMagnitude(c):
-    mag = tf.abs(c)
-    pha = tf_angle(c)
-    
-    sqrtmag = tf.sqrt(tf.nn.relu(mag))
-
-    magCompl = tf.complex(sqrtmag, tf.zeros(sqrtmag.shape))
-    phaCompl = tf.complex(tf.zeros(pha.shape), pha)
-    
-    return magCompl * tf.exp(phaCompl)
+    return powMagnitude(c, 0.5)
     
 def powMagnitude(c, power):
-    # mag = tf.abs(c)
-    # pha = tf_angle(c)
-    
-    # sqrtmag = tf.pow(tf.nn.relu(mag), power)
-
-    # magCompl = tf.complex(sqrtmag, tf.zeros(sqrtmag.shape))
-    # phaCompl = tf.complex(tf.zeros(pha.shape), pha)
-    
-    # return magCompl * tf.exp(phaCompl)
     return applyConstantToComplex(c, magFun = tf.pow, magConstant = power)
     
 def noEffectApplyConstant(c, constant):
