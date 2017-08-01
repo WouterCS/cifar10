0725aa069d909e0283678238d5a5eb4e3e89c35f
Wouter Uijens <wouter.cs@gmail.com>
2017-08-01 13:16:00 +0200

    Try absolute value in the new setting

--- models/cifar10/cifar10_params.py
+++ models/cifar10/cifar10_params.py
@@ -32,18 +32,10 @@ def main(runNum, directory):
     hyperParam.poolingFun = 'average-pool'
     hyperParam.INITIAL_LEARNING_RATE = 0.1
     
-    if runNum == 0:
-        print('run zero')
-        hyperParam.FIXED_LR = False
-        hyperParam.max_steps = 200000
-        hyperParam.convNonLin = 'powMagnitude'
-        hyperParam.convNonLinMag = 2.0
-        createReadMe(hyperParam)
-        return hyperParam
-        
     hyperParam.max_steps = 30000
-    hyperParam.convNonLin = 'powMagnitude'
-    hyperParam.convNonLinMag = 2 + np.random.random(1)[0]
+    hyperParam.convNonLin = 'absFFT'
+    hyperParam.convNonLinMag = 2.0
+    createReadMe(hyperParam)
     
     createReadMe(hyperParam)
     return hyperParam
