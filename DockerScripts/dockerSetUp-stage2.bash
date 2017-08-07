3b472346a67e1b9e600b5087f37e62827973e131
Wouter Uijens <wouter.cs@gmail.com>
2017-08-07 11:56:36 +0200

    Run variety of pow-magnitude, multiply-angle combinations (attempt 6)

--- DockerScripts/dockerSetUp-stage2.bash
+++ DockerScripts/dockerSetUp-stage2.bash
@@ -19,11 +19,11 @@ docker start $NAME
 docker exec -it $NAME mkdir -p /results
 docker exec -it $NAME cd /models/cifar10
 
-for i in {1..1000}
+for i in {1..1}
 do
     numRun=$i
     DIRECTORY='/results/run-'$numRun
     docker exec -it $NAME mkdir -p $DIRECTORY
-    docker exec -it $NAME python -c "import cifar10_params; hyperParam = cifar10_params.main(${numRun},'${DIRECTORY}'); import cifar10_train; cifar10_train.main(hyperParam, '${DIRECTORY}')" 2>&1  | tee "${DIRECTORY}/log.txt"
+    docker exec -it $NAME (python -c "import cifar10_params; hyperParam = cifar10_params.main(${numRun},'${DIRECTORY}'); import cifar10_train; cifar10_train.main(hyperParam, '${DIRECTORY}')" 2>&1  | tee "${DIRECTORY}/log.txt")
 done 
 
