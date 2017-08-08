8067a952bc2ad32b0b07a092cf88a9ffe6d24be0
Wouter Uijens <wouter.cs@gmail.com>
2017-08-08 13:53:02 +0200

    try functions on carhesian coordinates (attemot 2)

--- models/cifar10/cifar10.py
+++ models/cifar10/cifar10.py
@@ -49,7 +49,7 @@ from tensorflow.python.ops.spectral_ops import irfft2d, irfft
 
 import cifar10_input
 
-from custom_python_ops.composite_ops import powMagnitude, sqrtMagnitude, applyConstantToComplex
+from custom_python_ops.composite_ops import powMagnitude, sqrtMagnitude, applyConstantToComplex, applyConstantToComplexCart
 
 FLAGS = tf.app.flags.FLAGS
 
