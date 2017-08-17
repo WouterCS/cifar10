d9740e7d1a4fd62b3cff7397ef4c37256cf56f54
Wouter Uijens <wouter.cs@gmail.com>
2017-08-17 12:14:22 +0200

    Nightly run with various functions on the carthesian coordinates (attempt 2)

--- models/cifar10/cifar10_params.py
+++ models/cifar10/cifar10_params.py
@@ -53,27 +53,27 @@ def main(runNum, directory):
     hyperParam.steps_done_at_start = 0
     
     NumRepeatExps = 3   
-    fun_to_try = [tf.sigmoid, tf.tanh, tf.abs, tf.nn.elu] #tf.nn.relu
+    fun_to_try = [tf.sigmoid, tf.tanh, tf.abs, tf.nn.elu, tf.nn.softsign, tf.nn.softplus ] #tf.nn.relu
     
     current_fun = fun_to_try[ (runNum / NumRepeatExps) % len(fun_to_try) ]
     
     if runNum % NumRepeatExps == 0:
         hyperParam.non_linearity['conv']['type_of_nonlin'] = 'applyToRealOfComplex'
-        hyperParam.non_linearity['conv']['apply_const_function'] = lambda x, c: tf.nn.relu(x)
+        hyperParam.non_linearity['conv']['apply_const_function'] = lambda x, c: current_fun(x)
         hyperParam.non_linearity['conv']['const'] = 0.5
         
     if runNum % NumRepeatExps == 1:
         hyperParam.non_linearity['conv']['type_of_nonlin'] = 'applyToCartOfComplex'
-        hyperParam.non_linearity['conv']['apply_const_function'] = lambda x, c: tf.nn.relu(x)
+        hyperParam.non_linearity['conv']['apply_const_function'] = lambda x, c: current_fun(x)
         hyperParam.non_linearity['conv']['const'] = 0.5
-        hyperParam.non_linearity['conv']['secondary_const_fun'] = lambda x, c: tf.nn.relu(x)
+        hyperParam.non_linearity['conv']['secondary_const_fun'] = lambda x, c: current_fun(x)
         hyperParam.non_linearity['conv']['secondary_const'] = 2.0
         
     if runNum % NumRepeatExps == 2:
         hyperParam.non_linearity['conv']['type_of_nonlin'] = 'applyToCartOfComplex'
         hyperParam.non_linearity['conv']['apply_const_function'] = lambda x, c: x
         hyperParam.non_linearity['conv']['const'] = 0.5
-        hyperParam.non_linearity['conv']['secondary_const_fun'] = lambda x, c: tf.nn.relu(x)
+        hyperParam.non_linearity['conv']['secondary_const_fun'] = lambda x, c: current_fun(x)
         hyperParam.non_linearity['conv']['secondary_const'] = 2.0
         
     # if runNum % NumRepeatExps == 3:
