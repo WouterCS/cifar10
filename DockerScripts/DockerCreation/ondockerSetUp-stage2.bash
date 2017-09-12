88ed794cfae482e55be45a0965bae24f858043ea
Wouter Uijens <wouter.cs@gmail.com>
2017-09-12 14:51:42 +0200

    Added strided pooling, running extensive test in spatial domain (attempt 17)

--- DockerScripts/DockerCreation/ondockerSetUp-stage2.bash
+++ DockerScripts/DockerCreation/ondockerSetUp-stage2.bash
@@ -2,10 +2,10 @@ mkdir -p /results
 cd /models/cifar10
 
 echo '******************'
-echo '* start from 8 *'
+echo '* start from 5 *'
 echo '******************'
 
-for i in {8..200}
+for i in {5..200}
 do
     numRun=$i
     DIRECTORY='/results/run-'$numRun
