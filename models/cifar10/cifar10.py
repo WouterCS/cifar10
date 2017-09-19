997a3087f490467583a90439683fafb25ad99a0d
Wouter Uijens <wouter.cs@gmail.com>
2017-09-19 17:19:04 +0200

    Make possible training non-linearities with multiple weights (attempt 3)

--- models/cifar10/cifar10.py
+++ models/cifar10/cifar10.py
@@ -296,13 +296,13 @@ def inference(images, hyperParam):
                                           , tf.constant_initializer(hyperParam.non_linearity['conv']['const'])))
   trainable_const = tf.Print(trainable_const[0], trainable_const, message = '')
   
-  trainable_const1 = [_variable_on_cpu('trainable_const1_layer1', [1], tf.constant_initializer(hyperParam.non_linearity['conv']['const'])),
-                      _variable_on_cpu('trainable_const1_layer2', [1], tf.constant_initializer(hyperParam.non_linearity['conv']['const']))]
-  #trainable_const1 = tf.clip_by_value(trainable_const1, hyperParam.non_linearity['conv']['clip_min'], hyperParam.non_linearity['conv']['clip_max'])
-  trainable_const2 = [_variable_on_cpu('trainable_const2', [1], tf.constant_initializer(hyperParam.non_linearity['conv']['secondary_const'])),
-                      _variable_on_cpu('trainable_const2', [1], tf.constant_initializer(hyperParam.non_linearity['conv']['secondary_const']))]
-  #trainable_const2 = tf.clip_by_value(trainable_const2, hyperParam.non_linearity['conv']['clip_min'], hyperParam.non_linearity['conv']['clip_max'])
-  trainable_const1 = tf.Print(trainable_const1[0], [trainable_const1, trainable_const2], message = 'Current value trained non-lin:')
+  # trainable_const1 = [_variable_on_cpu('trainable_const1_layer1', [1], tf.constant_initializer(hyperParam.non_linearity['conv']['const'])),
+                      # _variable_on_cpu('trainable_const1_layer2', [1], tf.constant_initializer(hyperParam.non_linearity['conv']['const']))]
+  # #trainable_const1 = tf.clip_by_value(trainable_const1, hyperParam.non_linearity['conv']['clip_min'], hyperParam.non_linearity['conv']['clip_max'])
+  # trainable_const2 = [_variable_on_cpu('trainable_const2', [1], tf.constant_initializer(hyperParam.non_linearity['conv']['secondary_const'])),
+                      # _variable_on_cpu('trainable_const2', [1], tf.constant_initializer(hyperParam.non_linearity['conv']['secondary_const']))]
+  # #trainable_const2 = tf.clip_by_value(trainable_const2, hyperParam.non_linearity['conv']['clip_min'], hyperParam.non_linearity['conv']['clip_max'])
+  # trainable_const1 = tf.Print(trainable_const1[0], [trainable_const1, trainable_const2], message = 'Current value trained non-lin:')
   with tf.variable_scope('conv1') as scope:
     kernel = _variable_with_weight_decay('weights',
                                          shape=[5, 5, 3, 64],
