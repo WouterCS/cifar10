160f40c29cdebde87e7081cf6ee3fd33b3c99ba0
Wouter Uijens <wouter.cs@gmail.com>
2017-09-20 15:55:09 +0200

    Try training third degree polynomials on magnitude (attempt 13)

--- DockerScripts/DockerCreation/ondockerSetUp-stage2.bash
+++ DockerScripts/DockerCreation/ondockerSetUp-stage2.bash
@@ -11,5 +11,5 @@ do
     DIRECTORY='/results/run-'$numRun
     mkdir -p $DIRECTORY
     python -c "import cifar10_params; hyperParam = cifar10_params.main(${numRun},'${DIRECTORY}'); import cifar10_train; cifar10_train.main(hyperParam, '${DIRECTORY}')" 2>&1  | tee "${DIRECTORY}/log.txt"
-    sleep 5s
+    sleep 2s
 done 
