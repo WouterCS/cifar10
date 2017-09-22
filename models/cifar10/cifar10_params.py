416971229668ecd219d7d78afb759ea97edc8310
Wouter Uijens <wouter.cs@gmail.com>
2017-09-22 15:53:52 +0200

    Try result of long run as initialization for shot run (attempt 4)

--- models/cifar10/cifar10_params.py
+++ models/cifar10/cifar10_params.py
@@ -65,7 +65,7 @@ def main(runNum, directory):
     
     
     
-    tests = {0: {'FC_non_lin': 'identity', 'conv_non_lin': 'full_taylor' , 'pooling_function': 'average-pool', 'learn_const': True, 'degree': 2, 'conv_const': [[4.3757038, 2.6943564, 4.0667019], [1.1400001, 2.3812811, 0.66915196]]},}
+    tests = {0: {'FC_non_lin': 'identity', 'conv_non_lin': 'full_taylor' , 'pooling_function': 'average-pool', 'learn_const': True, 'degree': 2, 'conv_const': [[4.5283766 , 2.7704711 , 4.0667071], [0.26456073 , 2.4040191 , 0.66916424]]},}
              # 0: {'FC_non_lin': 'identity', 'conv_non_lin': 'funMagnitude', 'pooling_function': 'average-pool', 'conv_const': lambda x: (x*2-1) * max_init, 'learn_const': True, 'degree': 3},
              # 1: {'FC_non_lin': 'identity', 'conv_non_lin': 'full_taylor', 'pooling_function': 'average-pool', 'conv_const': lambda x: x * max_init      , 'learn_const': True, 'degree': 3},
              # 2: {'FC_non_lin': 'identity', 'conv_non_lin': 'full_taylor', 'pooling_function': 'average-pool', 'conv_const': lambda x: (x*2-1) * max_init, 'learn_const': True, 'degree': 4},
